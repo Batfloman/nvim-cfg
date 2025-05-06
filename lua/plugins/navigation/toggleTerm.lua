12cb3c763d9faa41aafba332e5bb4417df5d3a93
Batfloman <flhirche+github@gmail.com>
2025-05-06 20:16:46 +0200

    Added toggleTerm plugin

---
+++ lua/plugins/navigation/toggleTerm.lua
@@ -0,0 +1,27 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = '*',
+  config = function()
+    require('toggleterm').setup {
+      direction = 'float',
+    }
+
+    vim.keymap.set({ 'n', 't' }, '<C-T>', function()
+      require('toggleterm').toggle()
+    end, { desc = 'Toggle Terminal' })
+
+    vim.keymap.set({ 'n', 't' }, '<leader>Tc', function()
+      local Terminal = require('toggleterm.terminal').Terminal
+      local dir = vim.fn.expand '%:p:h'
+
+      local term = Terminal:new {
+        name = 'Cool name',
+        dir = dir,
+        direction = 'float',
+      }
+      term:toggle()
+    end, { desc = '[T]erminal [c]reate (in file dir)' })
+
+    vim.keymap.set('n', '<leader>Tl', '<cmd>TermSelect<CR>', { desc = '[T]erminal [l]ist' })
+  end,
+}
