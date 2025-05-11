97b53a1532262c5e32add7344ee9ddc8e7a66cd8
Batfloman <flhirche+github@gmail.com>
2025-05-11 14:39:07 +0200

    better naming

--- lua/plugins/navigation/toggleTerm.lua
+++ lua/plugins/navigation/toggleTerm.lua
@@ -3,24 +3,34 @@ return {
   version = '*',
   config = function()
     require('toggleterm').setup {
+      dir = vim.fn.expand '%:p:h',
       direction = 'float',
+      on_create = function(t)
+        local dir = vim.fn.expand '%:p:h'
+        dir = string.match(dir, '~/(.+)//')
+        t.display_name = string.format('[Terminal %d] - %s', t.id, dir)
+      end,
     }
 
     vim.keymap.set({ 'n', 't' }, '<C-T>', function()
-      require('toggleterm').toggle()
-    end, { desc = 'Toggle Terminal' })
+      local count = vim.v.count > 0 and vim.v.count or nil
+      require('toggleterm').toggle(count)
+      -- reopen to see changes made by 'on_create'
+      require('toggleterm').toggle(count)
+      require('toggleterm').toggle(count)
+    end, { desc = 'Toggle Terminal (with optional count)' })
 
     vim.keymap.set({ 'n', 't' }, '<leader>Tc', function()
       local Terminal = require('toggleterm.terminal').Terminal
       local dir = vim.fn.expand '%:p:h'
 
       local term = Terminal:new {
-        display_name = dir,
         dir = dir,
         direction = 'float',
       }
+      term.display_name = string.format('[Terminal %d] - %s', term.id, dir)
+
       term:toggle()
-      -- vim.cmd('ToggleTermSetName ' .. dir)
     end, { desc = '[T]erminal [c]reate (in file dir)' })
 
     vim.keymap.set('n', '<leader>Tl', '<cmd>TermSelect<CR>', { desc = '[T]erminal [l]ist' })
