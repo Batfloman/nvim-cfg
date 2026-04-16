8c1063fb885551db20926a074557ce4476537872
Batfloman <flhirche+github@gmail.com>
2026-04-16 11:00:53 +0200

    added root fallback

--- lua/config/lsp/servers/clangd.lua
+++ lua/config/lsp/servers/clangd.lua
@@ -1,7 +1,18 @@
+local util = require 'lspconfig.util'
+
 return {
   cmd = { 'clangd' },
   filetypes = { 'c', 'cpp', 'objc', 'objcpp' },
-  root_dir = require('lspconfig.util').root_pattern('.clangd', '.clang-tidy', '.clang-format', 'compile_commands.json', 'compile_flags.txt', '.git'),
+  root_dir = function(bufnr, on_dir)
+    local fname = type(bufnr) == 'number' and vim.api.nvim_buf_get_name(bufnr) or bufnr
+    if fname == nil or fname == '' then
+      return
+    end
+
+    local root = util.root_pattern('.clangd', '.clang-tidy', '.clang-format', 'compile_commands.json', 'compile_flags.txt', '.git')(fname)
+      or vim.fs.dirname(fname)
+    on_dir(root)
+  end,
   capabilities = {
     offsetEncoding = { 'utf-16' },
   },
